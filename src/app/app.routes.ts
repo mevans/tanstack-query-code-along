7de4639e126e8d626ada8508a5aea67082093b23
Matthew Evans <matthew@evans99.co.uk>
2025-03-16 19:48:57 +0000

    feat: basic api service & types

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -8,14 +8,14 @@ export const routes: Routes = [
         path: '',
         pathMatch: 'full',
         loadComponent: () =>
-          import('./todo-list/todo-list.component').then(
+          import('./components/todo-list/todo-list.component').then(
             (m) => m.TodoListComponent,
           ),
       },
       {
         path: ':id',
         loadComponent: () =>
-          import('./todo-detail/todo-detail.component').then(
+          import('./components/todo-detail/todo-detail.component').then(
             (m) => m.TodoDetailComponent,
           ),
       },
---
+++ src/app/services/todo-api/todo-api.service.ts
@@ -0,0 +1,54 @@
+import { Injectable, signal } from '@angular/core';
+import { CreateTodoPayload, Todo } from '../../types/todo.type';
+import { delay, Observable, of, throwError } from 'rxjs';
+
+@Injectable({
+  providedIn: 'root',
+})
+export class TodoApiService {
+  private todos = signal<Todo[]>([]);
+
+  constructor() {
+    this.seedTodos();
+  }
+
+  getTodos(): Observable<Todo[]> {
+    return this.simulateApiCall(this.todos());
+  }
+
+  getTodoById(id: number): Observable<Todo> {
+    const todo = this.todos().find((todo) => todo.id === id);
+    if (!todo) {
+      return throwError(() => new Error(`Todo with id ${id} not found`));
+    }
+
+    return this.simulateApiCall(todo);
+  }
+
+  createTodo(payload: CreateTodoPayload): Observable<Todo> {
+    const newTodo: Todo = {
+      id: this.todos().length + 1,
+      title: payload.title,
+      completed: false,
+    };
+
+    this.todos.update((todos) => [...todos, newTodo]);
+    return this.simulateApiCall(newTodo);
+  }
+
+  private seedTodos(): void {
+    this.todos.set([
+      { id: 1, title: 'Buy milk', completed: false },
+      { id: 2, title: 'Buy eggs', completed: true },
+      { id: 3, title: 'Buy bread', completed: false },
+    ]);
+  }
+
+  private simulateApiCall<TReturn = null>(
+    returnValue: TReturn,
+  ): Observable<TReturn> {
+    // Random delay between 500ms and 1500ms
+    const randomDelay = Math.floor(Math.random() * 1000) + 500;
+    return of(returnValue).pipe(delay(randomDelay));
+  }
+}
