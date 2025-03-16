569afc8df8496669cb78295e6db6f9e3525b5464
Matthew Evans <matthew@evans99.co.uk>
2025-03-16 19:35:43 +0000

    chore: deps + prettier config

--- src/app/app.component.spec.ts
+++ src/app/app.component.spec.ts
@@ -24,6 +24,8 @@ describe('AppComponent', () => {
     const fixture = TestBed.createComponent(AppComponent);
     fixture.detectChanges();
     const compiled = fixture.nativeElement as HTMLElement;
-    expect(compiled.querySelector('h1')?.textContent).toContain('Hello, tanstack-query-code-along');
+    expect(compiled.querySelector('h1')?.textContent).toContain(
+      'Hello, tanstack-query-code-along',
+    );
   });
 });
